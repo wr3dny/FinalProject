643d3938d0e26de40d5f8d3abca60cf755d7e816
wr3dny <you@example.com>
2022-07-08 15:11:51 +0200

    'progressing'

--- Plan_B/tetris_game.py
+++ Plan_B/tetris_game.py
@@ -1,3 +1,5 @@
+import random
+
 class Figure:
     fig = [
         [[1, 5, 9, 13], [4, 5, 6, 7]], # prosty
@@ -5,11 +7,33 @@ class Figure:
         [[0, 1, 5, 6], [1, 2, 4, 5], [0, 4, 5, 9], [1, 4, 5, 8]], # zetka
         [[0, 1, 2, 6], [0, 1, 2, 4], [1, 5, 8, 9], [1, 5, 9, 10]], # elka
         [[1, 4, 5, 6], [4, 5, 6, 9], [1, 4, 5, 9], [1, 5, 6, 9]] #piramida
+
     ]
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.type = random.randint(0, len(self.fig) - 1)
+        self.color = random.randint(1, len(colors) -1)
+        self.rotation = 0
+
+    def image(self):
+        return self.fig[self.type][self.rotation]
+
+    def rotate(self):
+        self.rotation = (self.rotation + 1) % len(self.fig[self.type])
+
+class Tetris:
+    lvl = 2
+    score = 0
+    state = 'start'
+    field = []
+    height = 0
+    width = 0
+    x = 100
+    y = 60
+    zoom = 20
+    fig = None
 
 
 def main():
