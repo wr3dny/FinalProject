1a68a0e88a46724cf30345391fa8aaadc6d3a0ab
wr3dny <you@example.com>
2022-07-09 09:22:04 +0200

    working on

--- Plan_C/snake.py
+++ Plan_C/snake.py
@@ -1,14 +1,19 @@
 import pygame
 import time
+import colors_rgb
+import random
 
-seashell3 = (205, 197, 191)
+# print(pygame.font.get_fonts())
+
+screen = colors_rgb.seashell3
 limegreen = (50, 205, 50)
 red = (255, 0, 0)
+food_for_snake = colors_rgb.purple1
+close_screen_color = colors_rgb.black
+close_screen_text = colors_rgb.limegreen
 
-screen_color = seashell3
+screen_color = screen
 msg_color = red
-
-name = input('Please give me your name \n --->')
 
 pygame.init()
 
@@ -18,57 +23,78 @@ display_height = 400
 display = pygame.display.set_mode((display_width, display_height))
 display.fill(screen_color)
 pygame.display.update()
-pygame.display.set_caption(f'Plan C - if everything else fails \n Now {name} plays')
+pygame.display.set_caption(f'Plan C - if everything else fails')
 
-gameover = False
-
-x1 = display_width / 2
-y1 = display_height / 2
+game_over = False
 
 snake_size = 10
-
-x1_change = 0
-y1_change = 0
 
 clock = pygame.time.Clock()
 snake_speed = 10
 
-font_type = pygame.font.SysFont('calibri', 15)
+font_type = pygame.font.SysFont('franklingothicmedium', 15)
 
 
-def message(msg,color):
+def message(msg, color):
     mesg = font_type.render(msg, True, color)
-    display.blit(mesg, [display_width / 2 , display_height / 2])
+    display.blit(mesg, [display_width / 4, display_height / 2])
+
+def gameloop():
+    game_over = False
+    game_close = False
+
+# snake starting position on screen 
+    x1 = display_width / 2
+    y1 = display_height / 2
+
+    x1_change = 0
+    y1_change = 0
+
+    food_for_snakex = round(random.randrange(0, display_width - snake_size))
+    food_for_snakey = round(random.randrange(0, display_height - snake_size))
+
+    while not game_over:
+        while game_close == True:
+            display.fill(close_screen_color)
+            message('You lost!\n Press "q" for quit or "a" for playing again', close_screen_text)
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_a:
+                        gameloop()
 
 
-while not gameover:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameover = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x1_change = -snake_size
-                y1_change = 0
-            elif event.key == pygame.K_RIGHT:
-                x1_change = snake_size
-                y1_change = 0
-            elif event.key == pygame.K_DOWN:
-                x1_change = 0
-                y1_change = snake_size
-            elif event.key == pygame.K_UP:
-                x1_change = 0
-                y1_change = -snake_size
 
-    if x1 >= display_width or x1 < 0 or y1 > display_height or y1 < 0:
-        gameover = True
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game_over = True
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        x1_change = -snake_size
+                        y1_change = 0
+                    elif event.key == pygame.K_RIGHT:
+                        x1_change = snake_size
+                        y1_change = 0
+                    elif event.key == pygame.K_DOWN:
+                        x1_change = 0
+                        y1_change = snake_size
+                    elif event.key == pygame.K_UP:
+                        x1_change = 0
+                        y1_change = -snake_size
 
-    x1 += x1_change
-    y1 += y1_change
+            if x1 >= display_width or x1 < 0 or y1 > display_height or y1 < 0:
+                game_over = True
 
-    display.fill(screen_color)
-    pygame.draw.rect(display, limegreen, [x1, y1, snake_size, snake_size])
-    pygame.display.update()
-    clock.tick(snake_speed)
+            x1 += x1_change
+            y1 += y1_change
+
+            display.fill(screen_color)
+            pygame.draw.rect(display, limegreen, [x1, y1, snake_size, snake_size])
+            pygame.display.update()
+            clock.tick(snake_speed)
 
 message('You lost', red)
 pygame.display.update()
---
+++ tttt.py
@@ -0,0 +1,11 @@
+import colors_rgb
+import random
+import pygame
+
+print(pygame.font.get_fonts())
+
+# color_list = colors_rgb
+# print(color_list)
+#
+# chosen = random.choices(color_list)
+# print(chosen)
